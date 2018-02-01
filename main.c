81420f7bb7a0fd9b8c7b58a16d5f7dda636f6035
Reece McTiernan CSM2016 <reece.mctiernan.2016@uni.strath.ac.uk>
2018-02-01 10:57:14 +0000

    Add parse and execute command

--- main.c
+++ main.c
@@ -1,48 +1,73 @@
 #include <stdio.h>
 #include <string.h>
 
-#define INPUT_SIZE 512
+#define MAX_INPUT_SIZE 512
 #define DELIMITER_LENGTH 8
+#define MAX_ARGUMENTS 50
+
 
 void getInput(char *input);
-void printTokens(char *input);
-void removeNewLine(char *input);
-
+void parse(char *input, char **arguments);
+int executeCommand(char **arguments);
 int main() {
+    int exitStatus = 0;
+    do {
+        char input[MAX_INPUT_SIZE] = {'\0'};
+        getInput(input);
+        char *arguments[MAX_ARGUMENTS];
+        parse(input, arguments);
+        
+        exitStatus = executeCommand(arguments);
+        
 
-    char input[INPUT_SIZE] = {'\0'};
-    getInput(input);
-    removeNewLine(input);
-    printTokens(input);
-    //printf("%s",input);
-    return 1;
+    } while (exitStatus != 1);
+    return 0;
 }
 
+/*
+ *  Gets input from the user
+ */
 void getInput(char *input) {
 
     printf("> ");
-    fgets(input, INPUT_SIZE, stdin);
+    fgets(input, MAX_INPUT_SIZE, stdin);
 
 }
 
-void printTokens(char *input) {
-
-    const char delimiters[] = {' ', '\t', '|','>','<','&',';','\0'};
+/*
+ * Tokenizes the input, storing each token into arguments
+ * Places NULL after the last token, into arguments
+ * Assumes arguments has space
+ */
+void parse(char *input, char **arguments) {
+    
+    const char delimiters[10] = " \t;<>|\n&";
     char* token;
 
-    token = strtok(input,delimiters);
+    token = strtok(input, delimiters);
+    int i = 0;
     while(token != NULL) {
-        printf("%s\n",token);
-        token = strtok(NULL,delimiters);
+        arguments[i] = token;
+        i++;
+        printf("(%s)\n", token);
+        token = strtok(NULL, delimiters);
+        
     }
+    arguments[i] = NULL;
 
 }
 
-void removeNewLine(char *input) {
-
-    char* pos;
-    if ((pos=strchr(input, '\n')) != NULL) {
-        *pos = '\0';
+/*
+ * Excutes the command
+ * Returns 1 if exiting
+ */
+int executeCommand(char **arguments) {
+    char *command = arguments[0];
+    //EOF (ctrl-D) or exit
+    if(command == NULL || strcmp("exit", command) == 0) {
+        return 1;
     }
+    
+    return 0;
 
 }
