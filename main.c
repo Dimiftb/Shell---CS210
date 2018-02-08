f668439e63ef22e68e3e6d583e9c3b42921acd2a
Dimitar Mihaylov <bukichoni@gmail.com>
2018-02-08 10:30:46 +0000

    Stage 3 and some stage 4 (clean please)

--- main.c
+++ main.c
@@ -10,44 +10,61 @@
 #define MAX_ARGUMENTS 50
 
 
-void getInput(char* input);
-void parse(char* input, char** arguments);
-void executeCommand(char** arguments);
-void execute(char** arguments);
+void getInput(char *input, char *pathog);
+void parse(char *input, char **arguments);
+int executeCommand(char **arguments);
+void execute(char **arguments);
+void getPath();
+void setPath(char **arguments);
+void setDirectory();
+void changeDirectory(char **arguments);
 
 int main() {
-
+    int exitStatus = 0;
+    char *pathog = getenv("PATH");
+    printf("Initial PATH test: %s\n", pathog);
+    setDirectory();
     do {
+        
         char input[MAX_INPUT_SIZE] = {'\0'};
-        getInput(input);
-        char* arguments[MAX_ARGUMENTS];
+        getInput(input, pathog);
+        char *arguments[MAX_ARGUMENTS];
         parse(input, arguments);
-        executeCommand(arguments);
-
-    } while (1);
+        exitStatus = executeCommand(arguments);
+       
+        if(exitStatus == 1) {
+            
+        }
+        
+    } while (exitStatus != 1);
+    setenv("PATH", pathog, 1);
+       printf("Last 'exit' PATH check: %s\n", getenv("PATH"));
     return 0;
 }
 
-/* 
- *   Gets input from the user
+/*
+ *  Gets input from the user
  */
-void getInput(char* input) {
+void getInput(char *input, char *pathog) {
 
     printf("> ");
     if(fgets(input, MAX_INPUT_SIZE, stdin) == NULL) {
+       setenv("PATH", pathog, 1);
+       printf("Last EOF PATH check: %s\n", getenv("PATH"));
         exit(0);
     }
 
 }
-/* 
- *  Tokenizes the input, storing each token into arguments
- *  Places NULL after the last token, into arguments
- *  Assumes arguments has space
+
+/*
+ * Tokenizes the input, storing each token into arguments
+ * Places NULL after the last token, into arguments
+ * Assumes arguments has space
  */
-void parse(char* input, char** arguments) {
+void parse(char *input, char **arguments) {
     
     const char delimiters[10] = " \t;<>|\n&";
-    char*  token;
+    char* token;
 
     token = strtok(input, delimiters);  
     int i = 0;
@@ -61,36 +78,51 @@ void parse(char* input, char** arguments) {
 	
 }
 
-/* 
- *  Excutes the command
- *  Returns 1 if exiting
+/*
+ * Excutes the command
+ * Returns 1 if exiting
  */
-void executeCommand(char** arguments) {
-    char* command = arguments[0];
-    //Ensure we're not going to dereference a null pointer
+int executeCommand(char **arguments) {
+    char *command = arguments[0];
+    //EOF (ctrl-D) 
     if(arguments[0] == NULL){
-       return;
+       return 0;
     }
+    //exit
     else if(strcmp("exit", command) == 0) {
-       exit(0);
-    } else {
-        //Non internal command
+        return 1;
+    } 
+    else if(strcmp("getpath", command) == 0) {
+        getPath();
+        return 0;
+    }
+    else if(strcmp("setpath", command) == 0) {
+        setPath(arguments);
+        return 0;
+    }
+    else if(strcmp("cd", command) == 0) {
+        changeDirectory(arguments);
+        return 0;
+    }
+    else {
+        //Non internal command 
         execute(arguments);
     }
-
+    return 0;
 }
 
-/* 
- *   Creates a child process, and executes the given command 
+/*
+ *  Creates a child process, and executes the given command 
  */
-void execute(char** arguments) {
+void execute(char **arguments) {
     pid_t pid = fork();
     if (pid < 0) {
         //Error if pid < 0
         printf("Error forking\n");
     } else if (pid > 0) {
         //Parent process
-        wait(NULL);
+        int status;
+        wait(&status);
     } else {
         //Child process
         //Use execvp so we can pass arguments, and it checks the PATH
@@ -100,5 +132,61 @@ void execute(char** arguments) {
         //Just incase
         exit(0);
     }
+}
 
+/*
+ * Built-in command prints the value of PATH.
+ */
+void getPath() {
+     
+    printf("PATH: %s\n" ,getenv("PATH"));
+     
+}
+
+/*
+ * Built-in command sets the value of PATH.
+ */
+void setPath(char **arguments) {
+    char *argument1 = arguments[1];
+    setenv("PATH", argument1, 1);
+    printf("Current PATH: %s\n", getenv("PATH"));
+}
+
+/*
+ * Set the cwd to HOME.
+ */
+void setDirectory() {
+    long size = 31;
+    char temp[128];
+    chdir(getenv("HOME"));
+    printf("Current working directory: %s/\n", getcwd(temp, (size_t)size));
+}
+/*
+ * Change the cwd
+ */
+void changeDirectory(char **arguments) {
+     long size = 31;
+    char temp[128];
+    char *argument1 = arguments[1];
+    if(strcmp(" ", argument1) == 0) {
+        printf("HOME: %s\n", getenv("HOME"));
+        chdir(getenv("HOME"));
+        printf("\n\n");
+        printf("Current working directory: %s/\n", getcwd(temp, (size_t)size));
+    }
+    else if(strcmp(".", argument1) == 0) {
+        chdir(".");
+        printf("Current working directory: %s/\n", getcwd(temp, (size_t)size));
+    }
+    else if(strcmp("..", argument1) == 0) {
+        chdir("..");
+        printf("Current working directory: %s/\n", getcwd(temp, (size_t)size));
+    }
+    
+    else {
+       if(chdir(argument1) == -1){
+           printf("Error: %s", strerror(errno));
+       }
+    }
+    
 }
